f1db1b5c697f80630600e894b237f59ed6f841e3
luizoti <luizlazoti@hotmail.com>
2020-10-08 17:26:11 -0300

    removed database script

--- API/login.py
+++ API/login.py
@@ -36,25 +36,4 @@ class LoginTrakt():
           "client_secret": api.keys['client_secret']
         }
         return api.ApiBase.post(self, METHOD, self.headers, data=data).status_code
-
-# import sqlite3
-# from sqlite3 import Error
-
-# class DataBase():
-#     def __init__(self):
-#         self.db_file = join(dirname(dirname(realpath(sys.argv[0]))), 'database.db')
-
-#     def create_connection(self):
-#         try:
-#             _db = sqlite3.connect(self.db_file)
-#             print(sqlite3.version)
-#         except Error as e:
-#             print(e)
-
-#         _db.close()
-#         pass
-
-        
-# db = DataBase()
-# db.create_connection()
-# # /home/luiz/trakt-gui-project/trakt-gui/API/database.db            
+    
