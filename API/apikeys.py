a5ab6218018a1569fe2d4fe0cc1c1cb8bc882da2
luizoti <luizlazoti@hotmail.com>
2020-10-10 23:42:01 -0300

    all apikeys and url apis moved to apikeys.py

---
+++ API/apikeys.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+trakt = {
+    'APIURL': 'https://api.trakt.tv',
+    'client_id': '1d7cdcd61fb21cc496f115a2918183f1071d3a7f4587fa2348c2610438141bf6',
+    'client_secret': 'dd1a0195c253a7db1472ea7b2883711d86792bb27fada694673aeec670d98553',
+}
+
+tmdb = {
+    'apikey': 'c2462a25f98f1994a20af4f85f18386a',
+    'APIURL': 'https://api.themoviedb.org/3',
+    'config': '/configuration?api_key=',
+}
