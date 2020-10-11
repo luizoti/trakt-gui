caac6ecccc311b0cdcf099c2a2c2660158cba836
luizoti <luizlazoti@hotmail.com>
2020-10-11 00:01:07 -0300

    refactored to more simple use and work with changes in apikeys, apibase

--- API/search.py
+++ API/search.py
@@ -1,37 +1,37 @@
 #!/usr/bin/env python3
 
 import json
-import API.apibase as api
+from API.apibase import get
+from API.apikeys import trakt
 
 
-class SearchTrakt():
-    def __init__(self):
-        self.TYPES = {
-            'all'        : '/search/all?query=',
-            'movie_show' : '/search/movie,show?query=',
-            'movie'      : '/search/movie?query=',
-            'show'       : '/search/show?query=',
-            'episode'    : '/search/episode?query=',
-            'person'     : '/search/person?query=',
-            'list'       : '/search/list?query=',
-        }
+TYPES = {
+    'all'        : '/search/all?query=',
+    'movie_show' : '/search/movie,show?query=',
+    'movie'      : '/search/movie?query=',
+    'show'       : '/search/show?query=',
+    'episode'    : '/search/episode?query=',
+    'person'     : '/search/person?query=',
+    'list'       : '/search/list?query=',
+}
 
-        self.headers = {
-            'Content-Type': 'application/json',
-            'trakt-api-version': '2',
-            'trakt-api-key': api.keys['client_id'],
-        }
+headers = {
+    'Content-Type': 'application/json',
+    'trakt-api-version': '2',
+    'trakt-api-key': trakt['client_id'],
+}
 
 
-    def Search(self, _type, movie):
-        try: # .content
-            response = api.ApiBase.get(self, ''.join([self.TYPES[_type], movie]), self.headers)
-            if response.status_code == 200 and len(json.loads(response.content)) == 0:
-                result = 0
-            elif response.status_code == 200 and len(json.loads(response.content)) > 0:
-                result = json.loads(response.content)
-                pass
-            print(response.status_code)
-        except Exception as e:
-            raise e
-        return result
+def Search(_type, movie):
+    try:
+        response = get(trakt['APIURL'], ''.join([TYPES[_type], movie]), headers)
+
+        if response.status_code == 200 and len(json.loads(response.content)) == 0:
+            result = 0
+        elif response.status_code == 200 and len(json.loads(response.content)) > 0:
+            result = json.loads(response.content)
+            pass
+
+    except Exception as e:
+        raise e
+    return result
