386b2052926c6e15bc62ef538adaade68be5ba9b
luizoti <luizlazoti@hotmail.com>
2020-10-10 22:48:27 -0300

    Fixed ico mode in search list

--- UI/Search.py
+++ UI/Search.py
@@ -1,4 +1,5 @@
-from PyQt5.QtCore import QSize, Qt
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import QSize, Qt, QRect
 from PyQt5.QtGui import QIcon, QPixmap, QCursor
 
 from PyQt5.QtWidgets import QSlider, QVBoxLayout, QHBoxLayout
@@ -9,26 +10,76 @@ from PyQt5.QtWidgets import QWidget, QListView, QListWidget, QListWidgetItem
 from API.search import SearchTrakt
 
 
-class CustonItem(QWidget):
-    def __init__ (self, _size):
+
+class CustonWidgetItem(QWidget):
+    def __init__ (self, _size, cover):
         QWidget.__init__(self)
-        self.setMinimumSize(_size)
+        self.IcoBar = QHBoxLayout()
+        self.addToWatched = QToolButton()
+        self.addToWatched.setFixedSize(QSize(30, 30))
 
-        self.coverlabel = QLabel(self)
-        cover = QPixmap('/home/luiz/trakt-gui-project/trakt-gui/media/covertest.jpg')
-        self.cover = cover.scaled(_size)
-        self.coverlabel.setPixmap(self.cover)
+        self.addToCollection = QToolButton()
+        self.addToCollection.setFixedSize(QSize(30, 30))
 
-        self.PrimaryWidget = QVBoxLayout()
-        self.PrimaryWidget.addWidget(self.coverlabel)
-        self.PrimaryWidget.setContentsMargins(1.25, 1.25, 1.25, 1.25)
-        self.setLayout(self.PrimaryWidget)
+        self.addToList = QToolButton()
+        self.addToList.setFixedSize(QSize(30, 30))
+
+        self.setHate = QToolButton()
+        self.setHate.setFixedSize(QSize(30, 30))
+        
+        self.addToCollection.clicked.connect(self.test1)
+        self.addToList.clicked.connect(self.test2)
+        self.setHate.clicked.connect(self.test3)
+        self.addToWatched.clicked.connect(self.test4)
+
+        self.IcoBar.addWidget(self.addToCollection)
+        self.IcoBar.addWidget(self.addToList)
+        self.IcoBar.addWidget(self.setHate)
+        self.IcoBar.addStretch(1)
+        self.IcoBar.addStretch(1)
+        self.IcoBar.addWidget(self.addToWatched)        
+        # 
+        self.coverwidt = QVBoxLayout()
+        self.coverlabel = QLabel()
+        cover = QPixmap(cover)
+        cover.scaled(_size)
+        self.coverlabel.setFixedSize(_size)
+        self.coverlabel.setContentsMargins(1, 1, 1, 1) # left, top, right, bottom
+        self.coverlabel.setPixmap(cover)
+        self.coverlabel.setScaledContents(True)
+
+        self.coverwidt.addWidget(self.coverlabel)
+
+        self.icos = QVBoxLayout()
+
+        self.icos.addLayout(self.coverwidt)
+        self.icos.addLayout(self.IcoBar)
+
+        self.icos.setContentsMargins(1, 1, 1, 1)
+        self.setLayout(self.icos)
+
+
+    def test1(self):
+        print(id(self))
+        pass
+
+    def test2(self):
+        print(id(self))
+        pass
+
+    def test3(self):
+        print(id(self))
+        pass
+
+    def test4(self):
+        print(id(self))
+        pass
 
 
 class CustomList(QListWidget):
     def __init__ (self):
         QListWidget.__init__(self)
-
+        self.setSpacing(1)
         self.setViewMode(QListView.IconMode)
         self.setMovement(QListView.Static)
         self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
@@ -46,6 +97,7 @@ class CustonLineEdit(QLineEdit):
         self.lay = QHBoxLayout()
         self.lay.addStretch(1)
         self.lay.addWidget(self.SearchBtnInside)
+        self.setText('avengers')
         self.setLayout(self.lay)
 
 
@@ -62,6 +114,7 @@ class SizeSlider(QSlider):
 class SearchUI(QWidget):
     def __init__(self):
         QWidget.__init__(self)
+        self.cover = "/home/luiz/trakt-gui-project/trakt-gui/media/covertest.jpg"
         self.modeState = 1
         self.globalInitialItemSize = QSize(150, 225)
 
@@ -73,17 +126,16 @@ class SearchUI(QWidget):
         self.SearchTextBox = CustonLineEdit()
         self.SearchList = CustomList()
         self.ChangeListModeButton = QPushButton("Mudar modo da lista", self)
-        self.ChangeSelectionModeBtn = QPushButton("Me", self)
+        self.ChangeSelectionModeBtn = QPushButton("Ativar Mult Seleção", self)
         self.ReturnToDashboard = QPushButton('Trakt Dashboard', self)
         self.SizeSlider = SizeSlider()
 
         self.SizeSlider.valueChanged.connect(self.SlideChangeSize)
         self.SearchList.itemSelectionChanged.connect(lambda: self.index())
         # 
-        self.ChangeListModeButton.clicked.connect(lambda: self.ChangeChangeListModeButton())
+        self.ChangeListModeButton.clicked.connect(lambda: self.doChangeListModeButton())
         self.ChangeSelectionModeBtn.clicked.connect(lambda: self.ChangeSelectionMode())
-        self.SearchTextBox.SearchBtnInside.clicked.connect(
-            lambda: self.doSearch(self.globalInitialItemSize))
+        self.SearchTextBox.SearchBtnInside.clicked.connect(lambda: self.doSearch(self.globalInitialItemSize))
         # 
         self.VLayTop.addWidget(self.SearchTextBox)
         self.VLayTop.addWidget(self.ChangeListModeButton)
@@ -105,17 +157,17 @@ class SearchUI(QWidget):
 
         try:
             for item in self.search_data:
-                tk_id = item['movie']['ids']['trakt']
                 # instance custon widget item
-                myCustonitem = CustonItem(_size)
-                myCustonListItem = QListWidgetItem(self.SearchList)
-                myCustonListItem.setSizeHint(_size)
+                CustonItem = CustonWidgetItem(_size, self.cover)
+                CustonWitgetItemList = QListWidgetItem(self.SearchList)
+                # width + 2 para compensar content margin and height + 30 + 2 para compensar barra de icones e contentmargin
+                CustonWitgetItemList.setSizeHint((QSize(_size.width() + 2, _size.height() + 30 + 2)))
                 # add custon itens to custon widget list
-                self.SearchList.addItem(myCustonListItem)
-                self.SearchList.setItemWidget(myCustonListItem, myCustonitem)
+                self.SearchList.addItem(CustonWitgetItemList)
+                self.SearchList.setItemWidget(CustonWitgetItemList, CustonItem)
                 # store memory adress reference with id() func
-                if id(myCustonListItem) not in self.mapedListItens:
-                    self.mapedListItens[id(myCustonListItem)] = item
+                if id(CustonWitgetItemList) not in self.mapedListItens:
+                    self.mapedListItens[id(CustonWitgetItemList)] = item
                     pass
                 pass
         except Exception as e:
@@ -147,6 +199,7 @@ class SearchUI(QWidget):
         self.SearchList.clear()
 
         sizesDict = {
+                   # 100+ 150+
             0: QSize(150, 225),
             1: QSize(250, 375),
             2: QSize(350, 525),
@@ -159,7 +212,7 @@ class SearchUI(QWidget):
         pass
 
 
-    def ChangeChangeListModeButton(self):
+    def doChangeListModeButton(self):
         if self.modeState == 0:
             self.SearchList.setViewMode(QListView.IconMode)
             self.modeState = 1
