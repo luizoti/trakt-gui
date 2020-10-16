e993196819f57dd08f88451e67f977bd2613721f
luizoti <luizlazoti@hotmail.com>
2020-10-16 13:38:00 -0300

    removed imports

--- API/apikeys.py
+++ API/apikeys.py
@@ -1,6 +1,4 @@
-import requests
-import json
-
+# 
 trakt = {
     'APIURL': 'https://api.trakt.tv',
     'client_id': '1d7cdcd61fb21cc496f115a2918183f1071d3a7f4587fa2348c2610438141bf6',
@@ -8,7 +6,6 @@ trakt = {
 }
 
 tmdb = {
-    'apikey': 'c2462a25f98f1994a20af4f85f18386a',
     'APIURL': 'https://api.themoviedb.org/3',
-    'config': '/configuration?api_key=',
+    'apikey': 'c2462a25f98f1994a20af4f85f18386a',
 }
